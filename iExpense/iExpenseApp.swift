cd646b995b4e94adb5242ad8defa17b450776f99
Anthy Chen <koyun.chen@gmail.com>
2023-05-18 15:44:04 -0700

    Initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Anthy Chen on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
